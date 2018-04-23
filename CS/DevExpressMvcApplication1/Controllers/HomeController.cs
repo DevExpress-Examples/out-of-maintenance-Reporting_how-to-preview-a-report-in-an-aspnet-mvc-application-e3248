a891ff3aa74f7eb0c394daea17b5fe0c61f4f505
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DevExpressMvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+// ...
+
+namespace DevExpressMvcApplication1.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!";
+
+            // Add the report to a view data.
+            ViewData["Report"] = new DevExpressMvcApplication1.Reports.XtraReport1();
+
+            return View();
+        }
+
+        public ActionResult DocumentViewerPartial() {
+            ViewData["Report"] = new DevExpressMvcApplication1.Reports.XtraReport1();
+            return PartialView("DocumentViewerPartial");
+        }
+
+        public ActionResult ExportDocumentViewer() {
+            return DevExpress.Web.Mvc.DocumentViewerExtension.ExportTo(new DevExpressMvcApplication1.Reports.XtraReport1());
+        }
+
+    }
+}
